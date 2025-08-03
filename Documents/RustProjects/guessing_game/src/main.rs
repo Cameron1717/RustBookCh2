436f2f8b3752f8166f190233a18a2f91ec571c62
Anise <cameron.casebolt17@gmail.com>
2025-08-03 11:38:49 -0700

    fixed bug that would casued game to not use the new user input

--- Documents/RustProjects/guessing_game/src/main.rs
+++ Documents/RustProjects/guessing_game/src/main.rs
@@ -4,10 +4,10 @@ use std::io;
 
 fn main() {
     println!("enter a number");
-    let mut guess = String::new();
     let secret = rand::thread_rng().gen_range(1..=100);
     println!("secret: {secret}");
     loop {
+        let mut guess = String::new();
         println!("enter guess");
         io::stdin()
             .read_line(&mut guess)
