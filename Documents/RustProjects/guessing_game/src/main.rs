070fde077935f2d951ed4d081d1366b9047c9b84
Anise <cameron.casebolt17@gmail.com>
2025-08-03 10:59:58 -0700

    added random num functionality

--- Documents/RustProjects/guessing_game/src/main.rs
+++ Documents/RustProjects/guessing_game/src/main.rs
@@ -1,7 +1,11 @@
+use rand::Rng;
 use std::io;
+
 fn main() {
     println!("enter a number");
     let mut guess = String::new();
+    let secret = rand::thread_rng().gen_range(1..=100);
+    println!("secret: {secret}");
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read Line");
