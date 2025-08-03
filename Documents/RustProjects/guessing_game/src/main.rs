97030657278e0bd4b2f74949d0f99f9c45bcab5f
Anise <cameron.casebolt17@gmail.com>
2025-08-03 11:35:03 -0700

    added looping functionality to the game to allow for repeated guesses, added error handling on user guess input to prevent crash

--- Documents/RustProjects/guessing_game/src/main.rs
+++ Documents/RustProjects/guessing_game/src/main.rs
@@ -7,15 +7,24 @@ fn main() {
     let mut guess = String::new();
     let secret = rand::thread_rng().gen_range(1..=100);
     println!("secret: {secret}");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read Line");
-    let guess: u32 = guess.trim().parse().expect("please Type a Number");
+    loop {
+        println!("enter guess");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read Line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Higher"),
-        Ordering::Greater => println!("Lower"),
-        Ordering::Equal => println!("Win!"),
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Higher"),
+            Ordering::Greater => println!("Lower"),
+            Ordering::Equal => {
+                println!("Win!");
+                break;
+            }
+        }
+        println!("you entered:{guess}")
     }
-    println!("you entered:{guess}")
 }
