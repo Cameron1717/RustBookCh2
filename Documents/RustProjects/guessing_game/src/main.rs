2d935f1552360fcbbc5445272bfd03b2a7b110d5
Anise <cameron.casebolt17@gmail.com>
2025-08-03 09:52:06 -0700

    added user input

--- Documents/RustProjects/guessing_game/src/main.rs
+++ Documents/RustProjects/guessing_game/src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("enter a number");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read Line");
+    println!("you entered:{guess}")
 }
